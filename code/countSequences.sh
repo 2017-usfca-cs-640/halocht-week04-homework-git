aacc2547640416a772bca26b341d5e9f9ecd204d
Alfredo H Locht <halocht@gmail.com>
2017-10-04 15:15:30 -0700

    Fix syntax of script but keep getting error message for line 11 unexpected token near 'echo'

--- code/countSequences.sh
+++ code/countSequences.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
+ 
+# Writing script (for loop) to count number of fasta sequences in arbitrary files
+ 
+# Alfredo Hans Locht halocht@gmail.com ahlocht@dons.usfca.edu
+ 
+# 03Oct2017 but first tried on 18Sep2017
 
-# Creating code (for loop) to count number of sequences in arbitrary files
+for filename in "$@"
 
-# Alfredo Hans Locht halocht@gmail.com
+	echo  "The file $filename has $(grep -c '^>' $filename) sequences in it"
 
-# 18Sep2017
-
-cd 
-
-cd Desktop
-
-cd halocht-week04-homework-git
-
-cd data/raw_data
-
-# 
-for file in "$@" ;
-
-do 
-	grep "string you need to find" file | wc -l
 done
